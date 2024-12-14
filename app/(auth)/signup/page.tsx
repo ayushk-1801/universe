ffc131d574bb715ac896949decd4cb0d62aa146c
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-12-15 00:02:22 +0530

    centered auth pages

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,25 +1,25 @@
-"use client"
+"use client";
 
-import { useSession } from 'next-auth/react';
-import Auth from '@/components/Auth';
-import { redirect } from 'next/navigation';
+import { useSession } from "next-auth/react";
+import Auth from "@/components/Auth";
+import { redirect } from "next/navigation";
 
 const Home = () => {
-    const { data: session, status } = useSession();
+	const { data: session, status } = useSession();
 
-    if (status === 'loading') {
-        return <div>Loading...</div>;
-    }
+	if (status === "loading") {
+		return <div>Loading...</div>;
+	}
 
-    if (session) {
-        redirect('/home');
-    } else {
-        return (
-            <div className="">
-                <Auth type="signup" />
-            </div>
-        );
-    }
+	if (session) {
+		redirect("/home");
+	} else {
+		return (
+			<div className="flex w-screen justify-center mt-48">
+				<Auth type="signup" />
+			</div>
+		);
+	}
 };
 
 export default Home;
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
 "use client";
 
-import { useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
+	const { data: session } = useSession();
 
-  if (session) {
-    const { user } = session;
-    return (
-      <div className="bg-green-800">
-        hi name: {user.name}
-        <div>
-          <Button onClick={() => signOut()}>Sign out</Button>
-        </div>
-      </div>
-    );
-  } else {
-    return <div className="bg-red-800">bye bye bye...</div>;
-  }
+	if (session && session.user) {
+		const { user } = session;
+		return (
+			<div className="bg-green-800">
+				hi name: {user.name}
+				<div>
+					<Button onClick={() => signOut()}>Sign out</Button>
+				</div>
+			</div>
+		);
+	}
+	return <div className="bg-red-800">bye bye bye...</div>;
 }
